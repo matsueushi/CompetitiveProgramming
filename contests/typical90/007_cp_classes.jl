9082ab65bcc82c1911e4431ee8939faac3b74d0c
matsueushi <matsueushi@gmail.com>
2022-01-27 14:22:04 +0000

    cp_classes

---
+++ contests/typical90/007_cp_classes.jl
@@ -0,0 +1,26 @@
+function abs_rating(as, b)
+    b ≤ first(as) && return first(as) - b
+    b ≥ last(as) && return b - last(as)
+    lb, ub = 1, length(as)
+    while (ub - lb > 1)
+        mid = (lb + ub) ÷ 2
+        if b < as[mid]
+            ub = mid
+        else
+            lb = mid
+        end
+    end
+    min(abs(b - as[lb]), abs(b - as[ub]))
+end
+
+function cp_classes(as, bs)
+    sort!(as)
+    [abs_rating(as, b) for b in bs]
+end
+
+n = parse(Int64, readline())
+as = parse.(Int64, split(readline()))
+q = parse(Int64, readline())
+lines = readlines()
+bs = parse.(Int64, lines[1:q])
+println.(cp_classes(as, bs))
