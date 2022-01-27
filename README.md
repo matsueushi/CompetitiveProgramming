8f55d520b153ce0166f1df76486672172551e166
matsueushi <matsueushi@gmail.com>
2022-01-27 12:23:26 +0000

    smallest subsequence

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # ProgrammingChallenges
+競技プログラミングの色々な問題をJuliaで解く
 
 - [AtCoder Beginners Selection](contests/atcoder_beginners_selection/)
 - [競プロ典型 90 問](contests/typical90/)
---
+++ contests/typical90/006_smallest_subsequence.jl
@@ -0,0 +1,18 @@
+function subseq(k, s)
+    ans = ""
+    len = 0
+    i_s = 1
+    i_e = length(s) - k + 1
+    while (len < k)
+        c, j = findmin(s[i_s:i_e])
+        ans *= c
+        i_s += j
+        i_e += 1
+        len += 1
+    end
+    ans
+end
+
+n, k = parse.(Int64, split(readline()))
+s = readline()
+println(subseq(k, s))
