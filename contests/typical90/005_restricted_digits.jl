ea18447e1101a06cebea987460522c0bf817497d
matsueushi <matsueushi@gmail.com>
2022-01-29 08:12:41 +0000

    restricted_digits: ver 2

--- contests/typical90/005_restricted_digits.jl
+++ contests/typical90/005_restricted_digits.jl
@@ -1,22 +1,38 @@
-function rdigits(n, b, cs)
-    m = 10^9 + 7
+mutable struct RestrictedDigits
+    b::Int64
+    cs::Vector{Int64}
+    res::Vector{Int64}
+    table::Matrix{Int64}
+end
+
+function RestrictedDigits(b, cs)
     res = zeros(Int64, b)
     for c in cs
         res[1+c%b] += 1
     end
+    table = [(10 * bi + c) % b + 1 for bi in 0:b-1, c in cs]
+    RestrictedDigits(b, cs, res, table)
+end
 
-    nres = zeros(Int64, b)
-    for i in 2:n
-        for bi in 0:b-1
-            for c in cs
-                nex = (10 * bi + c) % b
-                nres[1+nex] += res[1+bi]
-                nres[1+nex] = nres[1+nex] % m
-            end
+function shift!(rs::RestrictedDigits)
+    m = 10^9 + 7
+    nextres = zeros(Int64, rs.b)
+    for bi in 1:rs.b
+        for ci in 1:length(rs.cs)
+            nex = rs.table[bi, ci]
+            nextres[nex] += rs.res[bi]
+            nextres[nex] = nextres[nex] % m
         end
-        res = nres
     end
-    first(res)
+    rs.res = nextres
+end
+
+function rdigits(n, b, cs)
+    rs = RestrictedDigits(b, cs)
+    for i in 2:n
+        shift!(rs)
+    end
+    first(rs.res)
 end
 
 function main()
@@ -25,7 +41,8 @@ function main()
     println(rdigits(n, b, cs))
 end
 
-# main()
+main()
+# rdigits(3, 6, [1, 5, 9])
 for i in 1:5
     @time rdigits(10^i, 957, [1, 2, 3, 5, 6, 7, 9])
 end
