4201c55a624eb588dd1b4fc70399053678235f06
matsueushi <matsueushi@gmail.com>
2022-02-04 12:38:35 +0000

    crossing segments: AC

--- contests/typical90/017_crossing_segments.jl
+++ contests/typical90/017_crossing_segments.jl
@@ -1,19 +1,60 @@
+struct BitIndexedTree
+    bit::Vector{Int64}
+    BitIndexedTree(n::Integer) = new(zeros(Int64, n))
+end
+
+function add!(b::BitIndexedTree, i::Integer, x::Int64)
+    n = length(b.bit)
+    while (i ≤ n)
+        b.bit[i] += x
+        i += i & (-i)
+    end
+end
+
+function sum(b::BitIndexedTree, i::Integer)
+    s = 0
+    while (i > 0)
+        s += b.bit[i]
+        i -= i & (-i)
+    end
+    s
+end
+
+struct IntervalBitIndexedTree
+    bit0::BitIndexedTree
+    bit1::BitIndexedTree
+    IntervalBitIndexedTree(n::Integer) = new(BitIndexedTree(n), BitIndexedTree(n))
+end
+
+function add!(ib::IntervalBitIndexedTree, l::Integer, r::Integer, x::Int64)
+    add!(ib.bit0, l, -x * (l - 1))
+    add!(ib.bit1, l, x)
+    add!(ib.bit0, r + 1, x * r)
+    add!(ib.bit1, r + 1, -x)
+end
+
+function sum(ib::IntervalBitIndexedTree, i::Integer)
+    sum(ib.bit1, i) * i + sum(ib.bit0, i)
+end
+
 function crossing(n, ls, rs)
     idx = sortperm(rs)
     ls = ls[idx]
     rs = rs[idx]
-    midcount = zeros(Int64, n)
-    midcount_current = zeros(Int64, n)
+    midcount = IntervalBitIndexedTree(n)
+    currentls = Int64[]
     r_current = first(rs)
     crosses = 0
     for (l, r) in zip(ls, rs)
         if r_current != r
+            for cl in currentls
+                add!(midcount, cl + 1, r_current - 1, 1)
+            end
+            empty!(currentls)
             r_current = r
-            midcount += midcount_current
-            fill!(midcount_current, 0)
         end
-        crosses += midcount[l]
-        midcount_current[l+1:r-1] .+= 1
+        crosses += sum(midcount, l) - sum(midcount, l - 1)
+        push!(currentls, l)
     end
     crosses
 end
