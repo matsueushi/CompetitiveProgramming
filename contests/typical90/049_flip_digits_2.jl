8e75b76b11379f7ea462df83243458668d1738fd
matsueushi <matsueushi@gmail.com>
2022-02-21 13:05:44 +0000

    flip digits 2 ac

--- contests/typical90/049_flip_digits_2.jl
+++ contests/typical90/049_flip_digits_2.jl
@@ -1,30 +1,48 @@
+struct UnionFind
+    par::Vector{Int64}
+    rank::Vector{Int64}
+end
+
+UnionFind(n) = UnionFind(collect(1:n), zeros(Int64, n))
+
+function find!(uf::UnionFind, x)
+    1 ≤ x ≤ length(uf.par) || return nothing
+    uf.par[x] == x && return x
+    uf.par[x] = find!(uf, uf.par[x])
+end
+
+function unite!(uf::UnionFind, x, y)
+    1 ≤ x ≤ length(uf.par) || return
+    1 ≤ y ≤ length(uf.par) || return
+    rx = find!(uf, x)
+    ry = find!(uf, y)
+    rx == ry && return
+    if uf.rank[rx] < uf.rank[ry]
+        uf.par[rx] = ry
+    else
+        uf.par[ry] = rx
+        uf.rank[rx] == uf.rank[ry] && (uf.rank[rx] += 1)
+    end
+end
+
+same!(uf, x, y) = find!(uf, x) == find!(uf, y)
+
 function solve(n, m, cs, ls, rs)
     ix = sortperm(cs)
     cs = cs[ix]
     ls = ls[ix]
     rs = rs[ix]
-    score = 0
-    rx = 0
-    for i in 1:n
-        found = false
-        for j in 1:m
-            (i < ls[j] || rs[j] < i) && continue
-            if !found
-                found = true
-                score += cs[j]
-                rx = rs[j]
-            end
-
-            if rs[j] ≤ rx
-                ls[j] = rs[j] + 1
-                rs[j] = rx
-            else
-                ls[j] = rx + 1
-            end
-        end
-        !found && return -1
+    uf = UnionFind(n + 1) # 0 ~ n
+    e = 0
+    c = 0
+    for i in 1:m
+        same!(uf, ls[i], rs[i] + 1) && continue
+        unite!(uf, ls[i], rs[i] + 1)
+        c += cs[i]
+        e += 1
+        e == n && break
     end
-    score
+    e < n ? -1 : c
 end
 
 function main()
