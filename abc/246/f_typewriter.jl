b8f6f29688cedc80fd0bac4c5e465324cd8208d8
matsueushi <matsueushi@gmail.com>
2022-04-18 12:51:37 +0000

    246 typewriter

--- abc/246/f_typewriter.jl
+++ abc/246/f_typewriter.jl
@@ -1,22 +1,30 @@
 const P = 998244353
 
 function solve(n, l, ss)
+    ss0 = Int[]
+    for s in ss
+        a = 0
+        for x in collect(s) .- 'a'
+            a |= 1 << x
+        end
+        push!(ss0, a)
+    end
     res = 0
     for i in 0:2^n-1
-        chars = Set{Char}()
-        init = false
+        chars = -1
         for j in 0:n-1
             iszero(i & 1 << j) && continue
-            if !init
-                chars = Set(collect(ss[j+1]))
-                init = true
+            if chars < 0
+                chars = ss0[j+1]
             else
-                chars = intersect(chars, collect(ss[j+1]))
+                chars &= ss0[j+1]
             end
         end
-        ai = powermod(length(chars), l, P)
-        res += (-1)^(count_ones(i) + 1) * ai
-        res = mod(res, P)
+        if chars > 0
+            ai = powermod(count_ones(chars), l, P)
+            res += (-1)^(count_ones(i) + 1) * ai
+            res = mod(res, P)
+        end
     end
     res
 end
