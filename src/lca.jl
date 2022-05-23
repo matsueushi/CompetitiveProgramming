54af07c51203b39a6730a41567c2a042d539989c
matsueushi <matsueushi@gmail.com>
2022-05-23 14:53:26 +0000

    Lca

---
+++ src/lca.jl
@@ -0,0 +1,72 @@
+module LcaModule
+
+import Base.==
+
+export Lca, find_lca, distance
+
+struct Lca
+    ancestor::Matrix{Int}
+    depth::Vector{Int}
+    max_depth::Int
+end
+
+==(x::Lca, y::Lca) = x.ancestor == y.ancestor && x.depth == y.depth && x.max_depth == y.max_depth
+
+function dfs!(lca::Lca, graph::Vector{Vector{Int}}, v::Int, par::Int, depth::Int)
+    lca.ancestor[1, v] = par
+    lca.depth[v] = depth
+    for u in graph[v]
+        u == par && continue
+        dfs!(lca, graph, u, v, depth + 1)
+    end
+end
+
+function Lca(graph::Vector{Vector{Int}}, root::Int)
+    n = length(graph)
+    max_depth = 1
+    while 1 << max_depth < n
+        max_depth += 1
+    end
+
+    ancestor = zeros(Int, max_depth, n)
+    depth = zeros(Int, n)
+    lca = Lca(ancestor, depth, max_depth)
+    dfs!(lca, graph, root, 0, 1)
+
+    for d in 1:lca.max_depth-1
+        for v in 1:n
+            if lca.ancestor[d, v] > 0
+                lca.ancestor[d+1, v] = lca.ancestor[d, lca.ancestor[d, v]]
+            else
+                lca.ancestor[d+1, v] = 0
+            end
+        end
+    end
+
+    lca
+end
+
+function find_lca(lca::Lca, u::Int, v::Int)
+    if lca.depth[u] > lca.depth[v]
+        u, v = v, u
+    end
+    for k in 1:lca.max_depth
+        if (lca.depth[v] - lca.depth[u]) >> (k - 1) & 1 != 0
+            v = lca.ancestor[k, v]
+        end
+    end
+    u == v && return u
+    for k in lca.max_depth:-1:1
+        lca.ancestor[k, u] == lca.ancestor[k, v] && continue
+        u = lca.ancestor[k, u]
+        v = lca.ancestor[k, v]
+    end
+    lca.ancestor[1, u]
+end
+
+function distance(lca::Lca, u::Int, v::Int)
+    w = find_lca(lca, u, v)
+    lca.depth[u] + lca.depth[v] - 2 * lca.depth[w]
+end
+
+end
---
+++ test/test_lca.jl
@@ -0,0 +1,34 @@
+using CompetitiveProgramming.LcaModule
+
+@testset "Lca" begin
+    graph = [Int[] for i in 1:8]
+    push!(graph[1], 2)
+    push!(graph[2], 1)
+    push!(graph[1], 3)
+    push!(graph[3], 1)
+    push!(graph[2], 4)
+    push!(graph[4], 2)
+    push!(graph[2], 5)
+    push!(graph[5], 2)
+    push!(graph[3], 6)
+    push!(graph[6], 3)
+    push!(graph[5], 7)
+    push!(graph[7], 5)
+    push!(graph[5], 8)
+    push!(graph[8], 5)
+    lca = Lca(graph, 1)
+    @test lca == Lca(
+        [
+            0 1 1 2 2 3 5 5
+            0 0 0 1 1 1 2 2
+            0 0 0 0 0 0 0 0
+        ],
+        [1, 2, 2, 3, 3, 3, 4, 4],
+        3,
+    )
+    @test find_lca(lca, 2, 1) == 1
+    @test find_lca(lca, 2, 6) == 1
+    @test find_lca(lca, 7, 8) == 5
+
+    @test distance(lca, 4, 3) == 3
+end
